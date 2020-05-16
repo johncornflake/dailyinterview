0433957df4c3a3ebf07c2a3889bdad0125e3db82
chad <chadeous@gmail.com>
2020-05-16 12:49:03 -0400

    adding imports

---
+++ imported-from-gmail/2020-05-14-primes.py
@@ -0,0 +1,30 @@
+Hi, here's your problem today. (You've reached the end of the problems for now - in the meanwhile, here is a random question. And visit 
+CoderPro
+ for more practice!) This problem was recently asked by Amazon:
+
+
+Given a positive integer 
+n
+, find all primes less than 
+n
+.
+Here's an example and some starter code:
+def
+ 
+find_primes
+(
+n
+):
+  
+
+# Fill this in.
+print
+(
+find_primes
+(
+14
+))
+
+# [2, 3, 5, 7, 11, 13]
+
+
